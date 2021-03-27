89c7936fc0125b4e83b2628b6c194d9d4d9b0cdd
EmanuelHark12 <emanuel.hark@gmail.com>
2021-03-26 21:32:42 -0300

    Change the package name and additional of 8 data-sets of pokémon type.

--- R/data.R
+++ R/data.R
@@ -64,3 +64,176 @@
 #' }
 #' @source https://pokeapi.co/.
 "ice"
+#' Dados sobre os pokemon do tipo Fogo
+#'
+#' A tabela, gerada a partir da função poke_type com o argumento 'fire'
+#'
+#' @format Uma tabela com 13 colunas:
+#' \describe{
+#'   \item{Nome}{Nome do Pokemon}
+#'   \item{Regiao}{Região do Pokemon localizado pela primeira vez.}
+#'   \item{Tipo Principal}{Tipo Principal do Pokemon}
+#'   \item{Tipo Secundario}{Tipo Secundario do Pokemon}
+#'   \item{hp}{O Hp base do Pokemon}
+#'   \item{attack}{O ataque base do Pokemon}
+#'   \item{defense}{A defesa base do Pokemon}
+#'   \item{special-attack}{O ataque especial base do Pokemon}
+#'   \item{special-defense}{A defesa especial base do Pokemon}
+#'   \item{speed}{A velocidade base do Pokemon}
+#'   \item{Peso}{O Peso base do Pokemon}
+#'   \item{Altura}{A Altura base do Pokemon}
+#'   \item{id}{O id dentro da pokemon dentro do pokeapi}
+#' }
+#' @source https://pokeapi.co/.
+"fire"
+#' Dados sobre os pokemon do tipo Fada
+#'
+#' A tabela, gerada a partir da função poke_type com o argumento 'fairy'
+#'
+#' @format Uma tabela com 13 colunas:
+#' \describe{
+#'   \item{Nome}{Nome do Pokemon}
+#'   \item{Regiao}{Região do Pokemon localizado pela primeira vez.}
+#'   \item{Tipo Principal}{Tipo Principal do Pokemon}
+#'   \item{Tipo Secundario}{Tipo Secundario do Pokemon}
+#'   \item{hp}{O Hp base do Pokemon}
+#'   \item{attack}{O ataque base do Pokemon}
+#'   \item{defense}{A defesa base do Pokemon}
+#'   \item{special-attack}{O ataque especial base do Pokemon}
+#'   \item{special-defense}{A defesa especial base do Pokemon}
+#'   \item{speed}{A velocidade base do Pokemon}
+#'   \item{Peso}{O Peso base do Pokemon}
+#'   \item{Altura}{A Altura base do Pokemon}
+#'   \item{id}{O id dentro da pokemon dentro do pokeapi}
+#' }
+#' @source https://pokeapi.co/.
+"fairy"
+#' Dados sobre os pokemon do tipo Noturno
+#'
+#' A tabela, gerada a partir da função poke_type com o argumento 'dark'
+#'
+#' @format Uma tabela com 13 colunas:
+#' \describe{
+#'   \item{Nome}{Nome do Pokemon}
+#'   \item{Regiao}{Região do Pokemon localizado pela primeira vez.}
+#'   \item{Tipo Principal}{Tipo Principal do Pokemon}
+#'   \item{Tipo Secundario}{Tipo Secundario do Pokemon}
+#'   \item{hp}{O Hp base do Pokemon}
+#'   \item{attack}{O ataque base do Pokemon}
+#'   \item{defense}{A defesa base do Pokemon}
+#'   \item{special-attack}{O ataque especial base do Pokemon}
+#'   \item{special-defense}{A defesa especial base do Pokemon}
+#'   \item{speed}{A velocidade base do Pokemon}
+#'   \item{Peso}{O Peso base do Pokemon}
+#'   \item{Altura}{A Altura base do Pokemon}
+#'   \item{id}{O id dentro da pokemon dentro do pokeapi}
+#' }
+#' @source https://pokeapi.co/.
+"dark"
+#' Dados sobre os pokemon do tipo Inseto
+#'
+#' A tabela, gerada a partir da função poke_type com o argumento 'bug'
+#'
+#' @format Uma tabela com 13 colunas:
+#' \describe{
+#'   \item{Nome}{Nome do Pokemon}
+#'   \item{Regiao}{Região do Pokemon localizado pela primeira vez.}
+#'   \item{Tipo Principal}{Tipo Principal do Pokemon}
+#'   \item{Tipo Secundario}{Tipo Secundario do Pokemon}
+#'   \item{hp}{O Hp base do Pokemon}
+#'   \item{attack}{O ataque base do Pokemon}
+#'   \item{defense}{A defesa base do Pokemon}
+#'   \item{special-attack}{O ataque especial base do Pokemon}
+#'   \item{special-defense}{A defesa especial base do Pokemon}
+#'   \item{speed}{A velocidade base do Pokemon}
+#'   \item{Peso}{O Peso base do Pokemon}
+#'   \item{Altura}{A Altura base do Pokemon}
+#'   \item{id}{O id dentro da pokemon dentro do pokeapi}
+#' }
+#' @source https://pokeapi.co/.
+"bug"
+#' Dados sobre os pokemon do tipo Pedra
+#'
+#' A tabela, gerada a partir da função poke_type com o argumento 'rock'
+#'
+#' @format Uma tabela com 13 colunas:
+#' \describe{
+#'   \item{Nome}{Nome do Pokemon}
+#'   \item{Regiao}{Região do Pokemon localizado pela primeira vez.}
+#'   \item{Tipo Principal}{Tipo Principal do Pokemon}
+#'   \item{Tipo Secundario}{Tipo Secundario do Pokemon}
+#'   \item{hp}{O Hp base do Pokemon}
+#'   \item{attack}{O ataque base do Pokemon}
+#'   \item{defense}{A defesa base do Pokemon}
+#'   \item{special-attack}{O ataque especial base do Pokemon}
+#'   \item{special-defense}{A defesa especial base do Pokemon}
+#'   \item{speed}{A velocidade base do Pokemon}
+#'   \item{Peso}{O Peso base do Pokemon}
+#'   \item{Altura}{A Altura base do Pokemon}
+#'   \item{id}{O id dentro da pokemon dentro do pokeapi}
+#' }
+#' @source https://pokeapi.co/.
+"rock"
+#' Dados sobre os pokemon do tipo Terra
+#'
+#' A tabela, gerada a partir da função poke_type com o argumento 'ground'
+#' @format Uma tabela com 13 colunas:
+#' \describe{
+#'   \item{Nome}{Nome do Pokemon}
+#'   \item{Regiao}{Região do Pokemon localizado pela primeira vez.}
+#'   \item{Tipo Principal}{Tipo Principal do Pokemon}
+#'   \item{Tipo Secundario}{Tipo Secundario do Pokemon}
+#'   \item{hp}{O Hp base do Pokemon}
+#'   \item{attack}{O ataque base do Pokemon}
+#'   \item{defense}{A defesa base do Pokemon}
+#'   \item{special-attack}{O ataque especial base do Pokemon}
+#'   \item{special-defense}{A defesa especial base do Pokemon}
+#'   \item{speed}{A velocidade base do Pokemon}
+#'   \item{Peso}{O Peso base do Pokemon}
+#'   \item{Altura}{A Altura base do Pokemon}
+#'   \item{id}{O id dentro da pokemon dentro do pokeapi}
+#' }
+#' @source https://pokeapi.co/.
+"ground"
+#' Dados sobre os pokemon do tipo Fantasma
+#'
+#' A tabela, gerada a partir da função poke_type com o argumento 'ghost'
+#' @format Uma tabela com 13 colunas:
+#' \describe{
+#'   \item{Nome}{Nome do Pokemon}
+#'   \item{Regiao}{Região do Pokemon localizado pela primeira vez.}
+#'   \item{Tipo Principal}{Tipo Principal do Pokemon}
+#'   \item{Tipo Secundario}{Tipo Secundario do Pokemon}
+#'   \item{hp}{O Hp base do Pokemon}
+#'   \item{attack}{O ataque base do Pokemon}
+#'   \item{defense}{A defesa base do Pokemon}
+#'   \item{special-attack}{O ataque especial base do Pokemon}
+#'   \item{special-defense}{A defesa especial base do Pokemon}
+#'   \item{speed}{A velocidade base do Pokemon}
+#'   \item{Peso}{O Peso base do Pokemon}
+#'   \item{Altura}{A Altura base do Pokemon}
+#'   \item{id}{O id dentro da pokemon dentro do pokeapi}
+#' }
+#' @source https://pokeapi.co/.
+"ghost"
+#' Dados sobre os pokemon do tipo Dragão
+#'
+#' A tabela, gerada a partir da função poke_type com o argumento 'dragon'
+#' @format Uma tabela com 13 colunas:
+#' \describe{
+#'   \item{Nome}{Nome do Pokemon}
+#'   \item{Regiao}{Região do Pokemon localizado pela primeira vez.}
+#'   \item{Tipo Principal}{Tipo Principal do Pokemon}
+#'   \item{Tipo Secundario}{Tipo Secundario do Pokemon}
+#'   \item{hp}{O Hp base do Pokemon}
+#'   \item{attack}{O ataque base do Pokemon}
+#'   \item{defense}{A defesa base do Pokemon}
+#'   \item{special-attack}{O ataque especial base do Pokemon}
+#'   \item{special-defense}{A defesa especial base do Pokemon}
+#'   \item{speed}{A velocidade base do Pokemon}
+#'   \item{Peso}{O Peso base do Pokemon}
+#'   \item{Altura}{A Altura base do Pokemon}
+#'   \item{id}{O id dentro da pokemon dentro do pokeapi}
+#' }
+#' @source https://pokeapi.co/.
+"dragon"
--- data-raw/dados.R
+++ data-raw/dados.R
@@ -9,3 +9,35 @@ usethis::use_data(fighting, overwrite = TRUE)
 iceize <- poke_type('ice')
 ice <- poke_df(iceize)
 usethis::use_data(ice, overwrite = TRUE)
+
+fireze <- poke_type('fire')
+fire <- poke_df(fireze)
+usethis::use_data(fire, overwrite = TRUE)
+
+fairyize <- poke_type('fairy')
+fairy <- poke_df(fairyize)
+usethis::use_data(fairy, overwrite = TRUE)
+
+darkize <- poke_type('dark')
+dark <- poke_df(darkize)
+usethis::use_data(dark, overwrite = TRUE)
+
+bugize <- poke_type('bug')
+bug <- poke_df(bugize)
+usethis::use_data(bug, overwrite = TRUE)
+
+rockize <- poke_type('rock')
+rock <- poke_df(rockize)
+usethis::use_data(rock, overwrite = TRUE)
+
+groundize <- poke_type('ground')
+ground <- poke_df(groundize)
+usethis::use_data(ground, overwrite = TRUE)
+
+ghostize <- poke_type('ghost')
+ghost <- poke_df(ghostize)
+usethis::use_data(ghost, overwrite = TRUE)
+
+dragonize <- poke_type('dragon')
+dragon <- poke_df(dragonize)
+usethis::use_data(dragon, overwrite = TRUE)
